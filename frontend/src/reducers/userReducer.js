724a84ed12320e649d391127e5c5bbcfa6dd0d05
danhdeng <hui_danny_deng@hotmail.com>
2021-10-27 20:44:54 -0400

    update User Reducer

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,22 +1,13 @@
-import axios from "axios";
 import {
-    CLEAR_ERRORS, DELETE_USER_FAILURE,
-    // RESET_PASSWORD_REQUEST,
-    // RESET_PASSWORD_SUCCESS,
-    // RESET_PASSWORD_FAILURE,
-    // ALL_USERS_REQUEST,
-    // ALL_USERS_SUCCESS,
-    // ALL_USERS_FAILURE,
-    DELETE_USER_REQUEST,
+    ALL_USERS_FAILURE, ALL_USERS_REQUEST,
+    ALL_USERS_SUCCESS, CLEAR_ERRORS,
+    DELETE_USER_FAILURE, DELETE_USER_REQUEST,
     DELETE_USER_RESET,
     DELETE_USER_SUCCESS, FORGOT_PASSWORD_FAILURE, FORGOT_PASSWORD_REQUEST,
     FORGOT_PASSWORD_SUCCESS, LOAD_USER_FAILURE, LOAD_USER_REQUEST,
     LOAD_USER_SUCCESS, LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT_FAILURE, LOGOUT_SUCCESS, REGISTER_USER_FAILURE, REGISTER_USER_REQUEST,
-    REGISTER_USER_SUCCESS, UPDATE_PASSWORD_FAILURE, UPDATE_PASSWORD_REQUEST,
-    UPDATE_PASSWORD_SUCCESS, UPDATE_PROFILE_FAILURE, UPDATE_PROFILE_REQUEST,
-    UPDATE_PROFILE_SUCCESS, UPDATE_USER_FAILURE, UPDATE_USER_REQUEST,
-    UPDATE_USER_SUCCESS, USER_DETAILS_FAILURE, USER_DETAILS_REQUEST,
-    USER_DETAILS_SUCCESS
+    REGISTER_USER_SUCCESS, RESET_PASSWORD_FAILURE, RESET_PASSWORD_REQUEST,
+    RESET_PASSWORD_SUCCESS, UPDATE_PASSWORD_FAILURE, UPDATE_PASSWORD_REQUEST, UPDATE_PASSWORD_RESET, UPDATE_PASSWORD_SUCCESS, UPDATE_PROFILE_FAILURE, UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_RESET, UPDATE_PROFILE_SUCCESS, UPDATE_USER_FAILURE, UPDATE_USER_REQUEST, UPDATE_USER_RESET, UPDATE_USER_SUCCESS, USER_DETAILS_FAILURE, USER_DETAILS_SUCCESS
 } from "../constants/userConstants";
 
 //User Reducer
@@ -130,6 +121,102 @@ export const profileReducer = (state = {}, action) => {
                 error: null,
             }
         default:
+            return state;
+    }
+};
+
+export const forgotPasswordReducer = (state = {}, action)=>{
+    switch (action.type) {
+        case FORGOT_PASSWORD_REQUEST:
+        case RESET_PASSWORD_REQUEST:
+            return{
+                ...state,
+                loading: true,
+                error:null
+            };
+        case FORGOT_PASSWORD_SUCCESS:
+            return{
+                ...state,
+                loading: false,
+                message: action.payLoad,
+            };
+        case RESET_PASSWORD_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                success: action.payLoad,
+            };
+        case RESET_PASSWORD_FAILURE:
+        case FORGOT_PASSWORD_FAILURE:
+            return{
+                ...state,
+                loading:false,
+                error: action.payLoad,
+            };
+        case CLEAR_ERRORS:
+            return{
+                ...state,
+                error: null,
+            };
+        default: 
+            return state;
+    }
+};
+
+export const allUsersReducer=(state={users: []}, action)=>{
+    switch(action.type){
+        case ALL_USERS_REQUEST:
+            return{
+                ...state,
+                loading:true,
+            };
+        case ALL_USERS_SUCCESS:
+            return{
+                ...state,
+                loading: false,
+                users: action.payLoad,
+            };
+        case ALL_USERS_FAILURE:
+            return{
+                ...state,
+                loading: false,
+                error: action.payLoad,
+            };
+        case CLEAR_ERRORS:
+            return{
+                ...state,
+                error: null,
+            };
+        default:
+            return state;
+    }
+};
+
+export const userDetailsReducer=(state = {user:{}}, action)=>{
+    switch (action.type) {
+        case USER_DETAILS_REQUEST:
+            return{
+                ...state,
+                loading:true,
+            };
+        case USER_DETAILS_SUCCESS:
+            return{
+                ...state,
+                loading:false,
+                user: action.payLoad,
+            };
+        case USER_DETAILS_FAILURE:
+            return{
+                ...state,
+                loading:false,
+                error: action.payLoad,
+            };
+        case CLEAR_ERRORS:
+            return{
+                ...state,
+                error: null,
+            };
+        default: 
             return state;
     }
 };
