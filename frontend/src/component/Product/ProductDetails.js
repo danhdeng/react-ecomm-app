38ed81462c0060934faab1cace8456d037dbc4c6
danhdeng <hui_danny_deng@hotmail.com>
2021-10-29 08:00:26 -0400

    add carousel, update product details

---
+++ frontend/src/component/Product/ProductDetails.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+import { useAlert } from 'react-alert';
+import { useDispatch, useSelector } from 'react-redux';
+import { Loader } from "../layout/Loader/Loader";
+import { MetaData } from '../MetaData';
+import './ProductDetails.css';
+
+export const ProductDetails = ({match}) => {
+    const dispatch =useDispatch();
+    const alert=useAlert();
+    const {product, loading, error}=useSelector((state)=>state.productDetails);
+
+    return (
+        <Fragment>
+            {loading} ?(<Loader />):
+            (<Fragment>
+                <MetaData title={`${product.name} -- ECOMMERCE`} />    
+            </Fragment>)
+        </Fragment>
+    )
+}
