2cd16c7eeeecd8b0ed3e24d51dcce45f968383ca
danhdeng <hui_danny_deng@hotmail.com>
2021-10-30 22:44:37 -0400

    add ProtectedRoute

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,35 @@
-import React, { useEffect } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import webfont from 'webfontloader';
+import { loadUser } from './actions/userAction';
 import './App.css';
-import { Header } from './component/layout/Header/Header.js';
-import { Footer } from './component/layout/Footer/Footer.js';
 import { Home } from './component/Home/Home';
+import { Footer } from './component/layout/Footer/Footer.js';
+import { Header } from './component/layout/Header/Header.js';
+import { store } from './store';
 
 function App() {
+  const {isAuthenticated, user}=useSelector((state) =>state.user);
+  const [stripeApiKey, setStripeApiKey] =useState("");
+
+  async function getStripeApiKey(){
+    const {data}=await axios.get("/api/v1/stripeaipkey");
+    setStripeApiKey(data.stripeApiKey);
+  }
   useEffect(() => {
     webfont.load({
       google: {
         families: ["Roboto", "Doroid Sans", "Chilanka"]
       },
     });
+    store.dispatch(loadUser());
+    getStripeApiKey();
   }, []);
+  
+  window.addEventListener("contextmenu", (e)=>e.preventDefault());
+
   return (
     <Router>
       <Header />
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,66 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productReducer } from './reducers/productReducer';
+import thunk from 'redux-thunk';
+import { cartReducer } from './reducers/cartReducer';
+import {
+    allOrdersReducer,
+    myOrdersReducer,
+    newOrderReducer,
+    orderDetailsReducer,
+    orderReducer
+} from './reducers/orderReducer';
+import {
+    newProductReducer,
+    newReviewReducer,
+    productDetailsReducer,
+    productReducer,
+    productReviewsReducer,
+    productsReducer,
+    reviewReducer
+} from './reducers/productReducer';
+import {
+    allUsersReducer,
+    forgotPasswordReducer,
+    profileReducer,
+    userDetailsReducer,
+    userReducer
+} from './reducers/userReducer';
+
+
+
+
+
 const reducer = combineReducers({
-    products: productReducer
+    products: productsReducer,
+    productDetails: productDetailsReducer,
+    user: userReducer,
+    profile: profileReducer,
+    forgotPassword: forgotPasswordReducer,
+    cart: cartReducer,
+    newOrder: newOrderReducer,
+    myOrders: myOrdersReducer,
+    orderDetails: orderDetailsReducer,
+    newReview: newReviewReducer,
+    newProduct: newProductReducer,
+    product: productReducer,
+    allOrders: allOrdersReducer,
+    order: orderReducer,
+    allUsers: allUsersReducer,
+    userDetails: userDetailsReducer,
+    productReviews: productReviewsReducer,
+    review: reviewReducer,
 });
 
-let initialState = {};
+let initialState = {
+    cart:{
+        cartItems: localStorage.getItem("cartItems") 
+        ? JSON.parse(localStorage.getItem("cartItems"))
+        : [], 
+        shippingInfo: localStorage.getItem("shippingInfo")
+        ? JSON.parse(localStorage.getItem("shippingInfo"))
+        : {},
+    },
+};
 
 const middleware = [thunk];
 
