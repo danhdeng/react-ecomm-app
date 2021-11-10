7cc2aabae6f7064f2b562c058f402f33a2afd8ff
Dan Deng <hui_danny_deng@hotmail.com>
2021-11-10 18:56:41 -0500

    add get admin products to the backend route, and fixed Dashboard issue

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -8,15 +8,18 @@ const {
     createProductReview,
     getProductReviews,
     deleteReview,
-    getAdminProduct,
+    getAdminProducts,
 } = require('../controllers/productController');
 
 const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.route('/products').get(getAllProducts);
-router.route('/product/:id').get(getProductDetails);
+router.route("/products").get(getAllProducts);
+
+router
+  .route("/admin/products")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAdminProducts);
 
 router
     .route('/admin/product/new')
@@ -25,6 +28,8 @@ router
 router.route('/admin/product/:id')
     .put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct)
     .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
+
+router.route("/product/:id").get(getProductDetails);
 
 router.route('/review')
     .put(isAuthenticatedUser, createProductReview);
--- frontend/src/component/Admin/Dashboard.js
+++ frontend/src/component/Admin/Dashboard.js
@@ -1,3 +1,4 @@
+import "./Dashboard.css";
 import { Typography } from '@material-ui/core';
 import { useEffect } from 'react';
 import { Doughnut, Line } from 'react-chartjs-2';
@@ -9,51 +10,53 @@ import { getAllUsers } from '../../actions/userAction';
 import { MetaData } from '../layout/MetaData';
 import { SideBar } from './SideBar';
 export const Dashboard = () => {
-    const dispatch= useDispatch();
-    const {products}=useSelector((state)=>state.products);
-    const {orders}=useSelector((state)=>state.allOrders);
-    const {users}=useSelector((state)=>state.allUsers);
-    let outOfStock=0;
-    products && products.forEach((product)=>{
-        if(product.Stock===0){
-            outOfStock +=1;
-        }
-    });
-    let totalAmount=0;
-    orders && orders.forEach((order)=>{
-        totalAmount += order.Price.totalPrice;
-    });
-
-    const lineState={
-        labels:["Intial Amount", "Amount Earned"],
-        datasets:[
-            {
-                label: "TOTAL AMOUNT",
-                backgroundColor: ["tomato"],
-                hoverBackgroundColor:["rgb(197,72,49)"],
-                data:[0, totalAmount],
-            },
-        ],
-    };
-
-    const doughnutState={
-        labels:["Out of stock", "InStock"],
-        datasets: [
-            {
-                backgroundColor:["#00A6B4", "#6800B4"],
-                hoverBackgroundColor:["#4B5000","#35014F"],
-                data: [outOfStock, products.length-outOfStock],
-            },
-        ],
-    }
+    const dispatch = useDispatch();
+    const { products } = useSelector((state) => state.products);
+    const { orders } = useSelector((state) => state.allOrders);
+    const { users } = useSelector((state) => state.allUsers);
 
     useEffect(() => {
         dispatch(getAdminProduct());
         dispatch(getAllOrders());
         dispatch(getAllUsers());
-    },[dispatch]);
+    }, [dispatch]);
 
-    return(
+    let outOfStock = 0;
+    products && products.forEach((product) => {
+        if (product.Stock === 0) {
+            outOfStock += 1;
+        }
+    });
+    let totalAmount = 0;
+    orders && orders.forEach((order) => {
+        totalAmount += order.totalPrice;
+    });
+
+    const lineState = {
+        labels: ["Initial Amount", "Amount Earned"],
+        datasets: [
+            {
+                label: "TOTAL AMOUNT",
+                backgroundColor: ["tomato"],
+                hoverBackgroundColor: ["rgb(197, 72, 49)"],
+                data: [0, totalAmount],
+            },
+        ],
+    };
+
+    const doughnutState = {
+        labels: ["Out of Stock", "InStock"],
+        datasets: [
+            {
+                backgroundColor: ["#00A6B4", "#6800B4"],
+                hoverBackgroundColor: ["#4B5000", "#35014F"],
+                data: [outOfStock, outOfStock],
+            },
+        ],
+    }
+
+
+    return (
         <div className="dashboard">
             <MetaData title="Dashboard - Admin Panel" />
             <SideBar />
@@ -71,11 +74,11 @@ export const Dashboard = () => {
                         </Link>
                         <Link to="/admin/orders">
                             <p>Orders</p>
-                            <p>{users && users.length}</p>
+                            <p>{orders && orders.length}</p>
                         </Link>
                         <Link to="/admin/users">
                             <p>Users</p>
-                            <p>{products && products.length}</p>
+                            <p>{users && users.length}</p>
                         </Link>
                     </div>
                 </div>
